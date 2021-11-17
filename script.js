4ca6fda017f98c4e564400fdc37b2eee21e709be
Aniket <aniketsaste96@gmail.com>
2021-11-17 11:16:21 +0530

    calculate

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+function wordLength() {
+    let wordCount = document.querySelector("#text-box").value.length;
+    let totalWord = wordCount;
+    document.getElementById("demo").innerHTML = totalWord;
+}
